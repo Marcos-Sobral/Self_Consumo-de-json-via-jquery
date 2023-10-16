9bdb6b5d186611226a985ea2d9b82d30cc5e4711
Marcos-Sobral <vini15oficial@gmail.com>
2023-10-16 06:48:56 -0300

    atividade js avançado

---
+++ docs/Code.js
@@ -0,0 +1,11 @@
+var anime = {
+    nome: "Naruto",
+    genero: "shoune, aventura, comédia",
+    estudio: "Toe Animetion",
+    descrição: "aasasasassa",
+    quantidadeEp: 300
+};
+
+for(let i = 0; i < anime.length; i++){
+  console.log(anime[0]);
+}
